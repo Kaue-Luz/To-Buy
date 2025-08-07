c0db4a960e41422f3e19a6d21fb073b1e05ac77b
Erik Kauê <kaue_erik122@hotmail.com>
2025-08-07 19:47:34 -0300

    Componentes - FlatList

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input/Index";
@@ -7,6 +7,7 @@ import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
+const ITEMS = Array.from({ length: 100 }).map((_, index) => String(index));
 
 export function Home() {
   return (
@@ -33,10 +34,16 @@ export function Home() {
             </TouchableOpacity>
           </View>
 
-          <Item
-            data={{ status: FilterStatus.DONE, description: "Café" }}
-            onStatus={() => console.log("Mudar o status")}
-            onRemove={() => console.log("remover")}
+          <FlatList
+            data={ITEMS}
+            keyExtractor={(item) => item}
+            renderItem={({ item }) => (
+              <Item
+                data={{ status: FilterStatus.DONE, description: item }}
+                onStatus={() => console.log("Mudar o status")}
+                onRemove={() => console.log("remover")}
+              />
+            )}
           />
         </View>
       </View>
