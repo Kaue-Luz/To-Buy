b0866299041c00173ec60797f2a769eaf49d4f56
Erik Kauê <kaue_erik122@hotmail.com>
2025-08-12 20:43:36 -0300

    Estados e Funcionalidades - Input Controlado

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -37,6 +37,10 @@ export function Home() {
 
     await itemsStorage.add(newItem);
     await itemsByStatus();
+
+    Alert.alert("Adicionado", `Adicionado ${description}`);
+    setFilter(FilterStatus.PENDING)
+    setDescription("");
   }
 
   async function itemsByStatus() {
@@ -64,6 +68,7 @@ export function Home() {
           <Input
             placeholder="O que você precisa comprar?"
             onChangeText={setDescription}
+            value={description}
           />
           <Button title="Adicionar" onPress={handleAdd} />
         </View>
