218b430bae7ee834f373e708a7b3db4be46fe6bd
Erik Kauê <kaue_erik122@hotmail.com>
2025-08-13 20:29:06 -0300

    Estados e Funcionalidades - Método Clear

--- src/storage/itemStorage.ts
+++ src/storage/itemStorage.ts
@@ -50,9 +50,19 @@ async function remove(id: string): Promise<void> {
   await save(updatedItems);
 }
 
+//metodo clear
+async function clear(): Promise<void> {
+  try {
+    await AsyncStorage.removeItem(ITEMS_STORAGE_KEY);
+  } catch (error) {
+    throw new Error("ITEMS_CLEAR: " + error);
+  }
+}
+
 export const itemsStorage = {
   get,
   getByStatus,
   add,
   remove,
+  clear,
 };
